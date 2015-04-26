9db685facb2863d10439cef90127673562b976a0
Ben Thorner <benthorner@outlook.com>
2015-04-26 23:18:54 +0100

    Refactor RoundTextBlock into FanTextBlock with Fan view model

---
+++ Code/RadialControls/TemplateControls/FanTextBlock.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Thorner.RadialControls.ViewModels;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Thorner.RadialControls.TemplateControls
+{
+    public class FanTextBlock : Control
+    {
+        private Grid _grid;
+        private Fan _fan;
+
+        #region Dependency Properties
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            "Text", typeof(string), typeof(FanTextBlock), new PropertyMetadata(default(string), ResetFan));
+
+        public static readonly DependencyProperty RadiusProperty = DependencyProperty.Register(
+            "Radius", typeof(double), typeof(FanTextBlock), new PropertyMetadata(default(double), UpdateFan));
+
+        public static readonly DependencyProperty AlignmentProperty = DependencyProperty.Register(
+            "Alignment", typeof(Fan.Position), typeof(FanTextBlock), new PropertyMetadata(default(Fan.Position), UpdateFan));
+
+        #endregion
+
+        public FanTextBlock()
+        {
+            DefaultStyleKey = typeof(FanTextBlock);
+        }
+
+        #region Properties
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public double Radius
+        {
+            get { return (double)GetValue(RadiusProperty); }
+            set { SetValue(RadiusProperty, value); }
+        }
+        
+        public Fan.Position Alignment
+        {
+            get { return (Fan.Position)GetValue(AlignmentProperty); }
+            set { SetValue(AlignmentProperty, value); }
+        }
+
+        #endregion
+
+        #region UIElement Overrides
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+
+            _grid = GetTemplateChild("PART_Grid") as Grid;
+
+            this.FanOut();
+        }
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            if (_grid == null) return new Size(0, 0);
+            var blocks = _grid.Children.OfType<TextBlock>();
+
+            var offset = blocks.Any() ? blocks.Max((block) =>
+            {
+                block.Measure(availableSize);
+
+                return Math.Max(
+                    block.DesiredSize.Width,
+                    block.DesiredSize.Height
+                );
+            }) : 0;
+
+            return new Size(
+                2 * Radius + offset, 2 * Radius + offset
+            );
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            if (_grid == null) return new Size(0, 0);
+            var blocks = _grid.Children.OfType<TextBlock>();
+
+            var offset = blocks.Any() ? blocks.Max((block) =>
+            {
+                return Math.Max(
+                    block.ActualWidth, block.ActualHeight
+                );
+            }) : 0;
+
+            if (_fan != null) _fan.Flourish();
+
+            return base.ArrangeOverride(
+                new Size(2 * Radius + offset, 2 * Radius + offset)
+            );
+        }
+
+        #endregion
+
+        #region Event Handlers
+
+        private static void ResetFan(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            ((FanTextBlock)sender).FanOut();
+        }
+
+        private static void UpdateFan(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            var fan = sender as FanTextBlock;
+            if (fan._fan == null) return;
+
+            fan._fan.Radius = fan.Radius;
+            fan._fan.Alignment = fan.Alignment;
+        }
+
+        #endregion
+
+        #region Private Members
+
+        private void FanOut()
+        {
+            if (_grid == null || Text == null) return;
+            
+            _grid.Children.Clear();
+
+            foreach(var letter in Text) 
+            {
+                _grid.Children.Add(
+                    new TextBlock { Text = letter.ToString() }
+                );
+            }
+
+            _fan = new Fan(_grid.Children.OfType<TextBlock>())
+            {
+                Radius = Radius, Alignment = Alignment
+            };
+        }
+
+        #endregion
+    }
+}
---
+++ Code/RadialControls/ViewModels/Fan.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Windows.Foundation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace Thorner.RadialControls.ViewModels
+{
+    public class Fan
+    {
+        public enum Position { Middle, Start, End };
+
+        private IEnumerable<FrameworkElement> _slats;
+
+        public Fan(IEnumerable<FrameworkElement> slats)
+        {
+            _slats = slats ?? new List<FrameworkElement>();
+        }
+
+        #region Properties
+
+        public Position Alignment { get; set; }
+
+        public double Radius { get; set; }
+
+        public double Offset { get; set; }
+
+        #endregion
+
+        public void Flourish()
+        {
+            var offset = Offset + AlignmentFactor();
+
+            for (var i = 0; i < _slats.Count(); i++)
+            {
+                var element = _slats.ElementAt(i);
+                var arc = ArcAngle(element, Radius);
+
+                if (i > 0)
+                {
+                    offset += arc / 2;
+                }
+
+                FanOut(element, Radius, offset);
+                offset += arc / 2;
+            }
+        }
+
+        #region Private Members
+
+        private double AlignmentFactor()
+        {
+            if (_slats.Count() == 0) return 0.0;
+
+            double offset = _slats.Sum(
+                (slat) => ArcAngle(slat, Radius)
+            );
+
+            offset -= ArcAngle(_slats.First(), Radius) / 2;
+            offset -= ArcAngle(_slats.Last(), Radius) / 2;
+
+            switch (Alignment)
+            {
+                case Fan.Position.Middle: return -offset / 2;
+                case Fan.Position.End: return -offset;
+            }
+
+            return 0.0;
+        }
+
+        private void FanOut(FrameworkElement element, double radius, double angle)
+        {
+            var origin = new Point { X = 0.5, Y = 0.5 };
+            element.RenderTransformOrigin = origin;
+
+            element.RenderTransform = new TransformGroup
+            {
+                Children = new TransformCollection
+                    {
+                        new TranslateTransform { Y = - radius },
+                        new RotateTransform { Angle = angle }
+                    }
+            };
+
+            element.VerticalAlignment = VerticalAlignment.Center;
+            element.HorizontalAlignment = HorizontalAlignment.Center;
+        }
+
+        private double ArcAngle(FrameworkElement element, double radius)
+        {
+            return Math.Atan2(element.ActualWidth / 2, radius) * 360 / Math.PI;
+        }
+
+        #endregion
+    }
+}
