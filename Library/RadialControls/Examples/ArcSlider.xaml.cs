62c00660597eb8985696c797e4c9c1cf878bff2a
Ben Thorner <benthorner@outlook.com>
2015-07-07 22:58:15 +0100

    Add copyright/licence to everything

--- Library/RadialControls/Examples/ArcSlider.xaml.cs
+++ Library/RadialControls/Examples/ArcSlider.xaml.cs
@@ -1,11 +1,27 @@
-﻿using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Data;
-using Controls = Thorner.RadialControls.Controls;
+﻿/**
+ *  RadialControls - A circular controls library for Windows 8 Apps
+ *  Copyright (C) Ben Thorner 2015
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.If not, see<http://www.gnu.org/licenses/>.
+ **/
+
+using Thorner.RadialControls.Controls;
+using Windows.UI.Xaml;
 
 namespace Thorner.RadialControls.Examples
 {
-    public sealed partial class ArcSlider : Controls.Slider
+    public sealed partial class ArcSlider : Slider
     {
         public ArcSlider()
         {
